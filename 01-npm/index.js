78d8bf1ec130c2861c5ee5b76102ea11024e5403
lucaspod <lukasalvespod1@gmail.com>
2019-02-04 20:18:33 -0300

    first commit

---
+++ 01-npm/index.js
@@ -0,0 +1,37 @@
+"use strict";
+// console.log('Hello node!');
+// const a = 1
+// const b = 2
+// console.log(a+b);
+
+//1
+// class Usuario {
+//     retornarDadosUsuario(callback) {
+//         setTimeout(() => {
+//             return callback({
+//                 nome: "José Lucas"
+//             });
+//         });
+//     }
+// }
+
+// let usuario = new Usuario();
+// usuario.retornarDadosUsuario((resultado) => {
+//     console.log(resultado);
+// });
+
+//2
+// function retornarDadosUsuario(callback) {
+//     setTimeout(() => callback(
+//         { nome: "Jesus"}
+//     ));
+// }
+
+// function callbackDados(usuario) {
+//     console.log(usuario);
+// }
+
+// retornarDadosUsuario(callbackDados);
+
+
+
