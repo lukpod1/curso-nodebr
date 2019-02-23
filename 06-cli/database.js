5438a19503a88b62146c76a8d124b7037262855a
José Lucas <lukasalvespod1@gmail.com>
2019-02-23 12:02:02 -0300

    manipulando arquivos - DELETE

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -54,6 +54,20 @@ class Database {
         
         return dadosFiltrados;
     }
+
+    async remover(id){
+        if(!id) {
+            return await this.escreverArquivo([])
+        }
+
+        const dados = await this.obterDadosArquivos()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if(indice === -1) {
+            throw Error('O usuario informado não existe')
+        }
+        dados.splice(indice, 1)
+        return await this.escreverArquivo(dados)
+    }
 }
 
 module.exports = new Database()
